200e1d457811a8450f08d1ecec0263c190a38f17
Amber <amberbatool2026@gmail.com>
2026-02-20 15:50:51 -0800

    program

--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,18 @@
 
+x=2
+y=4
+result=x*y
+print("multiply= ",result)
+
+
+x=2
+y=4
+result=x*y
+print("multiply=",result)
+
+
+user_name="Qasim"
+user_name="Mujtaba"
+user=12
+user_nmae="Munna"
+print("user_name user_name user user_name=",result)
